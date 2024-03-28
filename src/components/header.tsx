43391553587e65bb09210629f0732be389d72828
Carlos Faustino <chfcchfc96@gmail.com>
2024-03-28 09:33:55 -0300

    feat: add header component

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <header className="flex w-full justify-center bg-header bg-cover bg-center bg-no-repeat pb-32 pt-16">
+      <img
+        src="/logo.png"
+        alt=""
+        width={148}
+      />
+    </header>
+  )
+}
